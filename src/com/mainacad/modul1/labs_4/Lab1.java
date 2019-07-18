afe2c974591ae432b09a79a493169a163a939eaa
Vadym Horiakin <vaginvest@gmail.com>
2019-07-18 22:00:38 +0300

    add labs

---
+++ src/com/mainacad/modul1/labs_4/Lab1.java
@@ -0,0 +1,18 @@
+package com.mainacad.modul1.labs_4;
+
+public class Lab1 {
+    public static void main(String[] args) {
+        byte b=12;
+        System.out.println("byte="+b);
+        short s=678;
+        System.out.println("sort="+s);
+        int i=54545454;
+        System.out.println("int="+i);
+        long l=45454544545L;
+        System.out.println("long="+l);
+        char c='e';
+        System.out.println("char="+c);
+        boolean bl=true;
+        System.out.println("boolean="+bl);
+    }
+}
